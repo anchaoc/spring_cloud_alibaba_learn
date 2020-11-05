5a94e27113c7c4dee22b8ced883c29a0c2c58814
安超 <240853850@qq.com>
2020-11-05 16:41:17 +0800

    init

--- shop-order/src/main/java/org/example/OrderApplication.java
+++ shop-order/src/main/java/org/example/OrderApplication.java
@@ -2,10 +2,15 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author anchao
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApplication {
 
     public static void main(String[] args) {
